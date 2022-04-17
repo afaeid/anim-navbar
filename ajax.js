649da93435a001807a96636fda1611e21200a939
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-04-17 16:15:14 +0600

    ok

---
+++ ajax.js
@@ -0,0 +1,31 @@
+
+  $(document).ready(function() {
+    $("#product").click(function() {
+      $("#mc-cvr").load("prdct.html");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#work").click(function() {
+      $("#mc-cvr").load("wrk.html p");
+    });
+  });
+  
+  $(document).ready(function() {
+    $("#team").click(function() {
+      $("#mc-cvr").load("tm.html p");
+    });
+  });
+
+  $(document).ready(function() {
+    $("#demo").click(function() {
+      $("#mc-cvr").load("demo.html p");
+    });
+  });
+
+
+  $(document).ready(function() {
+    $("#try").click(function() {
+      $("#mc-cvr").load("try.html p");
+    });
+  });
