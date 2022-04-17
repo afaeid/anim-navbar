9ae79b1939a380a29583de649dc0899b8f8b3937
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-04-17 16:42:52 +0600

    ok

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var header = document.querySelector('.header');
-var up = document.querySelector('.content');
+var cont = document.querySelector('.content');
 window.onscroll = ()=> {
   header.classList.toggle('new', window.scrollY > 20);
   if (window.scrollY > 20) {
-    up.style.marginTop = '90px';
+    cont.style.marginTop = '90px';
   }
   else{
-    up.style.marginTop = '20px';
+    cont.style.marginTop = '20px';
   }
 }
